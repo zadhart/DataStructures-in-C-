c3377b95439389a53567c501c2af2ef115448dca
zadhart <wams@ic.ufal.br>
2021-04-15 17:12:26 -0300

    Trying to make a graph code with adjacency matrix

--- main.cpp
+++ main.cpp
@@ -9,47 +9,57 @@ using namespace std;
 template <typename T>
 class Graph{
 private:
-    int size;
+    int graphSize;
 
 public:
-    vector<T> allNodes;
+    vector<T> allNodesData;
     vector<vector<int>> neighbors;
 
     Graph(){
-        size = 0;
+        this->graphSize = 0;
     }
 
-    void addVertex(T newData, int targetNode = -1){
-        this->allNodes.push_back(newData);
-        this->neighbors.push_back({targetNode});
+    void addEdge(int vertex1, int vertex2, int weight = 1){
+        this->neighbors[vertex1][vertex2] = weight;
+        this->neighbors[vertex2][vertex1] = weight;
+    }
 
-        if(this->size != 0){
-            this->neighbors[targetNode].push_back(this->size);
+    void addVertex(T newData, vector<int> targetNodes = {}){
+        this->allNodesData.push_back(newData);
+        this->neighbors.push_back(vector<int>(this->allNodesData.size()));
+
+        if(allNodesData.size() != 0){
+            //create the edges
+            for(int i = 0; i < this->allNodesData.size() - 1; i++){
+                this->neighbors[i].push_back(0);
+            }
         }
 
-        this->size++;
     }
 
 };
 
 int main() {
     Graph<string> my_graph;
-
     my_graph.addVertex("Greg");
-    my_graph.addVertex("Marcos", 0);
-    my_graph.addVertex("Higor", 0);
-    my_graph.addVertex("Elaine", 0);
-    my_graph.addVertex("Moises", 2);
+    my_graph.addVertex("Moises");
+    my_graph.addVertex("Rafa");
+    my_graph.addVertex("Perninha");
+    my_graph.addVertex("Ricardo");
 
-    cout << my_graph.allNodes[0] << "\n";
+    my_graph.addEdge(2,3);
+    my_graph.addEdge(4,0);
 
-    for(int i = 1; i < my_graph.neighbors[0].size(); i++){
-        cout << "    " << my_graph.allNodes[my_graph.neighbors[0][i]] << "\n";
+    for(int i = 0; i < my_graph.allNodesData.size(); i++){
+        cout << my_graph.allNodesData[i] << " ";
     }
+    cout << "\n";
 
-    cout << my_graph.allNodes[2] << "\n";
-    for(int i = 0; i < my_graph.neighbors[2].size(); i++){
-        cout << "    " << my_graph.allNodes[my_graph.neighbors[2][i]] << "\n";
+    for(int i = 0; i < my_graph.neighbors.size(); i++){
+        for(int x = 0; x < my_graph.neighbors[i].size(); x++){
+            cout << my_graph.neighbors[i][x] << " ";
+        }
+        cout << "\n";
     }
 
     return 0;
