50cb21f64ff552202ac83e7486af9787f5590a60
zadhart <wams@ic.ufal.br>
2021-04-16 21:13:47 -0300

    Trying to read a graph from a string

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ public:
         this->neighbors[vertex2][vertex1] = 0;
     }
 
-    void addVertex(T newData, vector<int> targetNodes = {}){
+    void addVertex(T newData = NULL, vector<int> targetNodes = {}){
         this->allNodesData.push_back(newData);
         this->neighbors.push_back(vector<int>(this->allNodesData.size()));
 
@@ -39,13 +39,39 @@ public:
                 this->neighbors[i].push_back(0);
             }
         }
-
     }
 
+    void readGraph(string g) {
+
+        //The code above find the number of vertex of a given graph
+        int y = 0;
+        for (int i = 0; i < g.size(); i++) {
+            if (g[i] == ';') {
+                y++;
+            }
+        }
+        y++;
+        cout << y << "\n";
+
+        cout << "g size: " << g.size() << "\n";
+
+        int init = 0;
+        int last = 9;
+
+        for(int i = 0; i < y; i++){
+            cout << "init: " << init << " last: " << last << "\n";
+            cout << g.substr(init, last) << "\n";
+            init = last + 1;
+            last = last + 10;
+        }
+    }
 };
 
 int main() {
     Graph<string> my_graph;
+    my_graph.readGraph("{0,0,0,3};{0,2,0,4};{0,0,0,0};{5,5,5,5}");
+
+    /*
     my_graph.addVertex("Greg");
     my_graph.addVertex("Moises");
     my_graph.addVertex("Rafa");
@@ -72,12 +98,26 @@ int main() {
 
     my_graph.removeEdge(3, 1);
 
+
+
     for(int i = 0; i < my_graph.neighbors.size(); i++){
         for(int x = 0; x < my_graph.neighbors[i].size(); x++){
             cout << my_graph.neighbors[i][x] << " ";
         }
         cout << "\n";
     }
+     */
+    /*
+    string g = "{0,0,0,0,1},{0,0,0,0,4},{0,0,0,4,2}";
+
+    for(int i = 0; i < g.size(); i++){
+        if(g[i] != '{'){
+            if(g[i] != '}'){
+                cout << g[i] << " ";
+            }
+        }
+    }
+    */
 
     return 0;
 }
